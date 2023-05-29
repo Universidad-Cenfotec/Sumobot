523689688e73c9a4d969939486bae7a7a08d62c2
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2023-05-29 16:15:52 -0600

    Update code_IR.py

--- code_IR.py
+++ code_IR.py
@@ -1,3 +1,9 @@
+# Tomás de Camino Beck
+# Escuela de Sistemas Inteligentes
+# Universidad Cenfotec
+
+# Ejemplo de uso del sensor infrarrojo conectado al IO33
+
 import board
 from ideaboard import IdeaBoard
 
