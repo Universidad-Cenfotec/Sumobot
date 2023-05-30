a9b4b4dc6da9fe56f81cf0851aa95c8fbe5508d8
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2023-05-30 07:32:51 -0600

    Update code_ultrasonic.py

--- code_ultrasonic.py
+++ code_ultrasonic.py
@@ -1,8 +1,15 @@
+# Tomás de Camino Beck
+# Escuela de Sistemas Inteligentes
+# Universidad Cenfotec
+
+#Ejemplo de uso del sensor ultrasónico
+
 import board #Librerías para accesar a I/O de la placa
 from ideaboard import IdeaBoard # Librería de funciones varias del ideaboard
 from time import sleep # Para utilizar función que detiene el código
 from hcsr04 import HCSR04 # Librerías para el sensor de Ultrasonido
 
+# TRIG conectado al pin IO26, y ECHO al Pin IO25
 sonar = HCSR04(board.IO26,board.IO25) # Crea una instanciación del sensor ultrasonido
 
 while True:
