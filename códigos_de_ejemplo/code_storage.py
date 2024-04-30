6bc1d45179950af905f21584f70ed1f73f7fb564
Tomás de Camino <166403865+tomascenfo@users.noreply.github.com>
2024-04-30 09:03:32 -0600

    Add files via upload
    Muestra como grabar archivos en el IdeaBoard

---
+++ códigos_de_ejemplo/code_storage.py
@@ -0,0 +1,27 @@
+#Tomás de Camino Beck
+#Universidad CENFOTEC
+import storage
+from ideaboard import IdeaBoard
+import time
+import board
+from adafruit_msa3xx import MSA311
+from hcsr04 import HCSR04 #
+
+sonar =  HCSR04(board.IO26,board.IO25)
+
+
+
+# Función para guardar datos
+def guardar_datos(archivo: str, datos):
+    with open(archivo, "a") as f:
+        f.write(",".join(str(d) for d in datos) + "\n")
+
+# Loop principal
+while True:
+    try:
+        distancia = sonar.dist_cm()
+        print("Distancia:", distancia, "cm")
+        guardar_datos("datos.csv",[distancia,distancia])
+    except RuntimeError as e:
+        print("Error al leer el sensor:", e)
+    time.sleep(0.2)  # Esperar un segundo antes de la próxima medición
