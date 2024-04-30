3216d868c8e6769f8c31fcf4d8f0d2c5e58cc91e
Tomás de Camino <166403865+tomascenfo@users.noreply.github.com>
2024-04-30 09:07:03 -0600

    se agregó docstring

--- códigos_de_ejemplo/code_storage.py
+++ códigos_de_ejemplo/code_storage.py
@@ -14,6 +14,16 @@ sonar =  HCSR04(board.IO26,board.IO25)
 
 # Función para guardar datos
 def guardar_datos(archivo: str, datos):
+    """
+    Guarda una lista de datos en un archivo CSV, añadiendo cada nueva entrada en una nueva línea.
+
+    Args:
+    archivo (str): El nombre del archivo donde se guardarán los datos.
+    datos (list): Lista de datos (cualquier tipo que pueda ser convertido a string) para ser escritos en el archivo.
+
+    Returns:
+    None
+    """   
     with open(archivo, "a") as f:
         f.write(",".join(str(d) for d in datos) + "\n")
 
