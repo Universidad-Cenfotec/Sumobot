84331447d7406b6772969abb85aef8e585b11fec
Tomás de Camino <166403865+tomascenfo@users.noreply.github.com>
2025-05-15 16:11:17 -0600

    Update code_ultrasonic.py

--- códigos_de_ejemplo/code_ultrasonic.py
+++ códigos_de_ejemplo/code_ultrasonic.py
@@ -9,8 +9,8 @@ from ideaboard import IdeaBoard # Librería de funciones varias del ideaboard
 from time import sleep # Para utilizar función que detiene el código
 from hcsr04 import HCSR04 # Librerías para el sensor de Ultrasonido
 
-# TRIG conectado al pin IO26, y ECHO al Pin IO25
-sonar = HCSR04(board.IO26,board.IO25) # Crea una instanciación del sensor ultrasonido
+# TRIG conectado al pin IO25, y ECHO al Pin IO26
+sonar = HCSR04(board.IO25,board.IO26) # Crea una instanciación del sensor ultrasonido
 
 while True:
     dist = sonar.dist_cm()
